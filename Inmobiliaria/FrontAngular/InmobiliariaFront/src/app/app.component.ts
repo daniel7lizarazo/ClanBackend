4e57e418f8687609b0602f3fdaa72d9d2cc9b319
daniel.lizarazo <daniel7lizarazo@gmail.com>
2021-11-29 11:36:51 -0500

    Intento de integración con angular, solo front

---
+++ Inmobiliaria/FrontAngular/InmobiliariaFront/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', '../assets/css/bootstrap.min.css','../assets/css/font-awesome.min.css','../assets/css/bootstrap-theme.css','../assets/css/style.css','../assets/css/camera.css']
+})
+export class AppComponent {
+  title = 'InmobiliariaFront';
+}
